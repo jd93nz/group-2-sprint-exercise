bd45f3a43b9347a7d75c4d2cb2b8277c7bb1a3e8
Jason Zhong <jasonzhong@developers.institute.co.nz>
2022-05-20 00:47:27 +1200

    add endpoint in the api

--- server/app.js
+++ server/app.js
@@ -5,8 +5,26 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use("/", () => {
-  console.log("HEY");
+
+// app.get("/", function (req, res) {
+//   res.send("I am foo");
+// });
+// app.get("/:name", function (req, res) {
+//   res.send("I am Foo with id " + req.params.name);
+// });
+
+app.get("/", (req, res) => {
+  res.send("Hello World !");
 });
 
+app.get("/:locationName", (req, res) => {
+  const location = req.params.locationName;
+  res.send(`Here is ${location}`);
+});
+
+app.get("/:locationName/:timeOption", (req, res) => {
+  const location = req.params.locationName;
+  const date = req.params.timeOption;
+  res.send(`Here is ${req.params.locationName} on ${req.params.timeOption}`);
+});
 module.exports = app;
