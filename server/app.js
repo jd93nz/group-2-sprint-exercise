974b40100dfec492b5002c2ec2491bda4a0577e8
Jason Zhong <jasonzhong@developers.institute.co.nz>
2022-05-20 09:45:57 +1200

    update endpoints

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const waterlevel = require("./water-level.json");
 
 app.use(cors());
 app.use(express.json());
@@ -10,14 +11,21 @@ app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
-app.get("/:locationName", (req, res) => {
+app.get("/water-depth", (req, res) => {
   const location = req.params.locationName;
-  res.send(`Here is ${location}`);
+  res.send(waterlevel);
 });
 
-app.get("/:locationName/:timeOption", (req, res) => {
+app.get("/water-depth/:locationName", (req, res) => {
+  const location = req.params.locationName;
+  res.send(`water level of ${location} today`);
+});
+
+app.get("/water-depth/:locationName/:timeOption", (req, res) => {
   const location = req.params.locationName;
   const date = req.params.timeOption;
-  res.send(`Here is ${req.params.locationName} on ${req.params.timeOption}`);
+  res.send(
+    `Water level of ${req.params.locationName} on ${req.params.timeOption}`
+  );
 });
 module.exports = app;
