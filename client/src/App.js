133986cd544577a24bfa27820cd2a868d092f04c
Jeffrey Dick <jeffrey@jeffreydick.com>
2022-05-20 14:10:38 +1200

    Get locations

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const url = "http://localhost:5001";
+  const getLocations = async () => {
+    const result = await fetch(`${url}/api/locations`);
+    const json = await result.json();
+    return json;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,8 +20,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
