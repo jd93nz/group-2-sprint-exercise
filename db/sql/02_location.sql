47c9d08e80037f394d1ae00348f832ad7ae2a234
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2022-05-20 15:35:01 +1200

    change 'title' to 'name'

--- db/sql/02_location.sql
+++ db/sql/02_location.sql
@@ -1,4 +1,4 @@
-INSERT INTO location (id, title, lat, lng) VALUES
+INSERT INTO location (id, name, lat, lng) VALUES
 (1, 'Dunedin', -45.8825661, 170.185258),
 (2, 'Auckland', -36.858931, 174.4252459),
 (3, 'Wellington', -41.2526167, 174.61383),
